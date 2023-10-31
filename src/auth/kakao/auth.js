da32cd60246f4984c8c952ecb5dedb18e7bb4bc0
iam454 <realseogy@gmail.com>
2023-10-31 20:34:59 +0900

    chore: 카카오 로그인 리다이렉트 uri 수정

--- src/auth/kakao/auth.js
+++ src/auth/kakao/auth.js
@@ -1,3 +1,4 @@
-const REDIRECT_URI = "http://localhost:3000/kakao/callback";
+const REDIRECT_URI =
+  "https://k2bf481c846ffa.user-app.krampoline.com/kakao/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
